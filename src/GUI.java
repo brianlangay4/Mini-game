0a967a298122dde630bf53167b345a3d42baf55e
Brian Langay <67788456+brianlangay4@users.noreply.github.com>
2022-08-31 19:17:35 +0800

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -1,118 +1,110 @@
 import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class GUI implements ActionListener{
-
+    //Putting all in scope before using them to give more function in other methods
+    //the guessed number
     Random number1 = new Random();
-
     int min = 1;
     int max = 4;
-
-    int i = ((number1.nextInt(max + min) + min));
-    int b1 = 1;
-    int b2 = 2;
-    int b3 = 3;
-    int b4 = 4;
-    int b5 = 5;
-
-
+    String i = String.valueOf(number1.nextInt(max + min) + min);
+    //used to obtain text field value
+    String perform1;
+    //j frame representatives
     JFrame frame;
-
     JLabel label;
-    JButton button1;
-    JButton button2;
-    JButton button3;
-    JButton button4;
-    JButton button5;
+    JLabel label2;
+    JTextField textField;
+    JLabel  Main;
+    JLabel  hint;
     JPanel panel;
+    JPanel panel1;
     public GUI (){
-
-        button1 = new JButton();
-        button1.setText("1");
-        button1.addActionListener(this);
-        button2 = new JButton();
-        button2.setText("2");
-        button2.addActionListener(this);
-        button3 = new JButton();
-        button3.setText("3");
-        button3.addActionListener(this);
-        button4 = new JButton();
-        button4.setText("4");
-        button4.addActionListener(this);
-        button5 = new JButton();
-        button5.setText("5");
-        button5.addActionListener(this);
-        frame = new JFrame();
-        label = new JLabel();
-        label.setText(" ");
-        label.setBounds(10,50,50,70);
-        panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(70,100, 100,100));
-        panel.setLayout(new GridLayout(0,1));
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
-        panel.add(button5);
-        panel.add(label);
-        panel.setVisible(true);
-
-
-
-        frame.add(panel,BorderLayout.CENTER);
-        frame.pack();
-        frame.setTitle("Mini Game");
-        frame.setVisible(true);
-
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+     //label1
+     Main = new JLabel();
+     Main.setText("number Guessing");
+     //label2
+     label2 = new JLabel();
+     //label3
+     label = new JLabel();
+     label.setText(" ");
+     perform1 = "";
+     //label3
+     hint = new JLabel();
+     hint.setText("HINT!(1-5)");
+     //text field
+     textField = new JTextField();
+     textField.setSize(50,50);
+     textField.addActionListener(this);
+     //button
+     JButton login = new JButton();
+     login.setText("Enter");
+     login.setBackground(Color.cyan);
+     login.addActionListener(this);
+     //first panel
+     panel1 = new JPanel();
+     panel1.setBorder(BorderFactory.createEmptyBorder(20,30, 20,30));
+     panel1.add(label2);
+     panel1.add(label);
+     //second panel
+     panel = new JPanel();
+     panel.setBorder(BorderFactory.createEmptyBorder(0,100, 50,100));
+     panel.setLayout(new GridLayout(0,1));
+     panel.add(textField);
+     panel.add(login);
+     panel.setVisible(true);
+     panel.add(Main);
+     panel.add(hint);
+     //frame components
+     frame = new JFrame();
+     frame.add(panel1,BorderLayout.BEFORE_FIRST_LINE);
+     frame.add(panel,BorderLayout.CENTER);
+     frame.pack();
+     frame.setTitle("Mini Game");
+     frame.setVisible(true);
+     frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
+
+    //main methods
     public static void main (String[]args){
         new GUI();
 
     }
-
+    //Action condition from implemented Action listener
     @Override
     public void actionPerformed(ActionEvent e) {
-
-
-       boolean loop = true;
+        //getting the text field
+       textField.setAction(textField.getAction());
+        perform1 = textField.getText();
+        //loop to maintain all condition
+        boolean loop = true;
         do {
-            if (button1.equals(i)){
-                System.out.println("");
-                label.setText("win");
+        //conditions
+            if (perform1.isBlank()){
+                System.out.println("no inputs");
+                label.setText("no input");
             }
-            if (button2.equals(i)){
-                System.out.println("");
-                label.setText("win");
-            }
-            if (button3.equals(i)){
-                System.out.println("");
-                label.setText("win");
-            }
-            if (button4.equals(i)){
-                System.out.println("");
-                label.setText("win");
-            }
-            if (button5.equals(i)){
-                System.out.println("");
-                label.setText("win");
-            }
+           if (perform1.equals(i)){
+               label.setText("");
+               label2.setText("you win");
+               System.out.println("you win");
+           }
             else {
-                System.out.println("");
-                label.setText("incorrect");
-            }
+                label2.setText("not correct");
+               System.out.println("not correct");
+           }
 
-        }while (loop = false);
+        } while (loop = false);
+
     }
 }
 
